98da703be54e3fd83d89eabf9894e0463390cc87
Jahongirhacking <Jahongirhacking@gmail.com>
2024-01-02 14:15:13 +0500

    feat: app is connected to DB

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 import express from "express";
 import cors from "cors";
-
-const PORT = 5000;
+import mongoose from "mongoose";
+import { PORT, DB_PATH } from "./config.js";
 
 const app = express();
 
 app.use(cors());
 
-app.listen(PORT, () => {
-  try {
-    console.log(`Server is running on ${PORT}`);
-  } catch (err) {
-    console.error(err);
-  }
+app.get("/", (req, res) => {
+  res.setHeader("Cache-Control", "no-store");
+  return res.status(234).send("Hello World!");
 });
+
+mongoose
+  .connect(DB_PATH)
+  .then(() => {
+    console.log("App is connected to the Database");
+    app.listen(PORT, () => {
+      try {
+        console.log(`Server is running on ${PORT}`);
+      } catch (err) {
+        console.error(err);
+      }
+    });
+  })
+  .catch((err) => console.error(err));
